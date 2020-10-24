02d03999809c6988e4d8458bc83814bb79684c6d
jallend1 <jallend1@gmail.com>
2020-10-24 14:44:03 -0700

    Adds a couple dummy functions to play around with declaring parameter types

--- public/app.js
+++ public/app.js
@@ -1,3 +1,14 @@
 "use strict";
 var inputs = document.querySelectorAll('input');
 console.log(inputs);
+var add = function (a, b, c) {
+    if (c === void 0) { c = 'jason'; }
+    console.log(a + b);
+    console.log(c);
+};
+var subtract = function (initial, newNumber, thirdNumber) {
+    thirdNumber ? console.log(initial - newNumber - thirdNumber) : console.log(initial - newNumber);
+};
+add(1, 5);
+add(1, 5, 'jack');
+subtract(5, 2, 10);
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
 const inputs = document.querySelectorAll('input');
 console.log(inputs);
+
+const add = (a: number, b:number, c: number | string = 'jason') => {
+    console.log(a + b);
+    console.log(c)
+}
+
+const subtract = (initial: number, newNumber: number, thirdNumber?: number) => {
+    thirdNumber ? console.log(initial - newNumber - thirdNumber) : console.log(initial - newNumber);
+}
+add(1, 5);
+add(1, 5, 'jack')
+subtract(5, 2, 10)
